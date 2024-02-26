582f4b8bdea1d37ef749038cbf808617aaca4870
PelinCoskun <pelinefpit@gmail.com>
2024-02-26 16:47:01 +0300

    sfd

---
+++ src/app.js
@@ -0,0 +1,15 @@
+//var,let,const
+//JS fire base(dosya bazlı)'dir.
+
+let sayi1 = 10;
+sayi1="Pelin Coskun"
+let student={id:1,name:"Pelin"}
+//console.log(student)
+
+function save(puan=10,ogrenci) {
+  console.log(ogrenci.name+ " : "+ puan)
+}
+save(undefined,student)
+
+let students=["Engin Demiroğ","Pelin Coşkun","İzel","Ece"]
+console.log(students)
