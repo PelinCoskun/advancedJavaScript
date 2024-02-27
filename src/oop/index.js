118b9647ea81c9d852c187a65c0fe99ffcfd5664
PelinCoskun <pelinefpit@gmail.com>
2024-02-27 17:04:42 +0300

    xxf

--- src/oop/index.js
+++ src/oop/index.js
@@ -1,3 +1,13 @@
 class Customer{
-    
+    constructor(id,customerNumber){
+
+    }
 }
+
+let customer=new Customer(1,"12345");
+//prototyping
+customer.name="Pelin Coşkun"
+console.log(customer.name)
+
+Customer.bisey="Bişey"
+console.log(customer.bisey)
