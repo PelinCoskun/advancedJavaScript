a849f1ad46dd3816513a79b291ac5485ca9e73a5
PelinCoskun <pelinefpit@gmail.com>
2024-02-27 17:18:32 +0300

    add

---
+++ src/mapFilterReduce/index.js
@@ -0,0 +1,7 @@
+let products = [
+    { id: 1, name: "Acer Laptop", unitPrice: 15000 },
+    { id: 2, name: "Monster Laptop", unitPrice: 16000 },
+    { id: 3, name: "Asus Laptop", unitPrice: 17000 },
+    { id: 4, name: "Dell Laptop", unitPrice: 18000 },
+    { id: 5, name: "Casper Laptop", unitPrice: 19000 },
+  ];
